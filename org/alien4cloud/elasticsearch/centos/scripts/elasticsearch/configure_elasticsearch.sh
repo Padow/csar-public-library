1a35e44734c3860b2328dafb8bfb9512949e92af
doucheme <patrick.douchement@fastconnect.fr>
2017-11-27 15:20:33 +0100

    ....

--- org/alien4cloud/elasticsearch/centos/scripts/elasticsearch/configure_elasticsearch.sh
+++ org/alien4cloud/elasticsearch/centos/scripts/elasticsearch/configure_elasticsearch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-ES_PATH=$INSTALL_PATH/elasticsearch
+ES_PATH=$INSTALL_PATH/elasticsearch-1.7.6
 if [[ $SECURED == "true" ]]; then
 	cd $ES_PATH/bin
 	./plugin --install search-guard-ssl --url https://fastconnect.org/maven/service/local/repositories/opensource/content/com/floragunn/search-guard-ssl/1.7.0/search-guard-ssl-1.7.0.zip
@@ -10,42 +10,42 @@ if [[ $SECURED == "true" ]]; then
 
 
 echo '
-cluster.name: ${CLUSTER_NAME}
+cluster.name: '${CLUSTER_NAME}'
 node.name: a4c_node1
 node.master: true
 node.data: true
 index.number_of_shards: 1
 index.number_of_replicas: 1
-path.logs: ${INSTALL_PATH}/elasticsearch/logs
+path.logs: '${ES_PATH}'/logs
 discovery.zen.ping.multicast.enabled: false
 discovery.zen.ping.unicast.enabled: true
-discovery.zen.ping.unicast.hosts: ${ES_IP}
+discovery.zen.ping.unicast.hosts: '${ES_IP}'
 searchguard.ssl.http.clientauth_mode: REQUIRE
 searchguard.ssl.http.enable_openssl_if_available: false
 searchguard.ssl.http.enabled: true
-searchguard.ssl.http.keystore_filepath: ${ES_PATH}/config/server-keystore.jks
+searchguard.ssl.http.keystore_filepath: '${ES_PATH}'/config/server-keystore.jks
 searchguard.ssl.http.keystore_password: changeit
-searchguard.ssl.http.truststore_filepath: ${ES_PATH}/config/server-truststore.jks
+searchguard.ssl.http.truststore_filepath: '${ES_PATH}'/config/server-truststore.jks
 searchguard.ssl.http.truststore_password: changeit
 searchguard.ssl.transport.enable_openssl_if_available: false
 searchguard.ssl.transport.enabled: true
 searchguard.ssl.transport.enforce_hostname_verification: true
 searchguard.ssl.transport.resolve_hostname: false
-searchguard.ssl.transport.keystore_filepath: ${ES_PATH}/server-keystore.jks
+searchguard.ssl.transport.keystore_filepath: '${ES_PATH}'/config/server-keystore.jks
 searchguard.ssl.transport.keystore_password: changeit
-searchguard.ssl.transport.truststore_filepath: ${ES_PATH}/server-truststore.jks
+searchguard.ssl.transport.truststore_filepath: '${ES_PATH}'/config/server-truststore.jks
 searchguard.ssl.transport.truststore_password: changeit
 ' > elasticsearch.yml
 else
 	cd $ES_PATH/config
 echo '
-cluster.name: ${CLUSTER_NAME}
+cluster.name: '${CLUSTER_NAME}'
 node.name: a4c_node1
 node.master: true
 node.data: true
 index.number_of_shards: 1
 index.number_of_replicas: 1
-path.logs: ${INSTALL_PATH}/elasticsearch/logs
+path.logs: '${ES_PATH}'/logs
 
 ' > elasticsearch.yml
 fi
--- org/alien4cloud/elasticsearch/centos/scripts/elasticsearch/start_elasticsearch.sh
+++ org/alien4cloud/elasticsearch/centos/scripts/elasticsearch/start_elasticsearch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
-cd $INSTALL_PATH/elasticsearch/bin
-./elasticsearch
+cd $INSTALL_PATH/elasticsearch-1.7.6/bin
+nohup ./elasticsearch > /dev/null 2>&1 &
+echo "ES launched"
